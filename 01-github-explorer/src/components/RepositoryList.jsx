71dc311ee626af41a77024dc1319f4a87e6735cb
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-12 20:24:32 -0300

    listing repositories

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -2,14 +2,6 @@ import { RepositoryItem } from "./RepositoryItem"
 import '../styles/repositories.scss'
 import { useState, useEffect } from "react";
 
-
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/'
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -23,18 +15,9 @@ export function RepositoryList() {
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem
-          repository={repository}
-        />
-        <RepositoryItem
-          repository={repository}
-        />
-        <RepositoryItem
-          repository={repository}
-        />
-        <RepositoryItem
-          repository={repository}
-        />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   );
