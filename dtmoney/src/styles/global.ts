dfebec8e9162f24a0a68d4605a8ce3b03496ba0f
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-14 20:43:17 -0300

    Creating global styles

---
+++ dtmoney/src/styles/global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root{
+    --background: #f0f2f5;
+    --red: #e52e4d;
+    --blue: #5429cc;
+
+    --blue-light: #6933ff;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --shape: #ffffff;
+  }
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  //formatando font de acordo com tamanho da tela
+  html{
+    @media(max-width: 1800px){
+      font-size: 93.75%; //15px
+    }
+    //REM -> irem = font-size(16px)
+
+    @media(max-width: 720px){
+      font-size: 87.5%; //14px
+    }
+  }
+
+  body{
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button{
+    cursor: pointer;
+  }
+
+  [disable]{
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
