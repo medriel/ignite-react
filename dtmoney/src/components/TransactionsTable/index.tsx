840a7c12100cc94d0aa92f696f1205e88b76f5cb
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-15 20:45:01 -0300

    Configuring Axios Client

--- dtmoney/src/components/TransactionsTable/index.tsx
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,12 +1,12 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionTable() {
 
   useEffect(() => {
-    fetch('http://localhost:3001/api/transactions')
-      .then(response => response.json())
-      .then(data => console.log(data))
+    api.get('transactions')
+      .then(response => console.log(response.data))
   }, []);
 
   return (
