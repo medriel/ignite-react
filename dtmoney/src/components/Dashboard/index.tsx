5585cc0002294a5e66c2fa4e388e4fea7717f886
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-15 19:45:59 -0300

    Component Summary

---
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from "../Summary/index";
+import { Container } from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  );
+}
