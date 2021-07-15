8c3e9f73f474eebe5ace88ec75b32bc8c857d0b2
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-15 20:36:16 -0300

    Configuring MirageJS

--- dtmoney/src/components/TransactionsTable/index.tsx
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function TransactionTable() {
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/transactions')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, []);
+
   return (
     <Container>
       <table>
