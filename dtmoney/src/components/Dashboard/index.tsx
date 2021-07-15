e489688f40664f87c97352c50849e74ee436a91e
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-15 20:07:30 -0300

    Component TransactionsTable

--- dtmoney/src/components/Dashboard/index.tsx
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary/index";
+import { TransactionTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionTable />
     </Container>
   );
 }
