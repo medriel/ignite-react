7f4ae6d7c42f7881b113ceafc6738972b231ed09
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-18 17:38:11 -0300

    Moving creation into context

--- dtmoney/src/TransactionsContext.tsx
+++ dtmoney/src/TransactionsContext.tsx
@@ -10,11 +10,29 @@ interface Transaction {
   createdAt: string;
 }
 
+// interface TransactionInput{
+//   title: string;
+//   amount: number;
+//   type: string;
+//   category: string;
+// }
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
+//type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type' | 'category' >;
+
 interface TransactionsProviderProps {
   children: ReactNode;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -24,8 +42,12 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then(response => setTransactions(response.data.transactions))
   }, []);
 
+  function createTransaction(transaction: TransactionInput) {
+    api.post('/transactions', transaction)
+  }
+
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
       {children}
     </TransactionsContext.Provider>
   );
