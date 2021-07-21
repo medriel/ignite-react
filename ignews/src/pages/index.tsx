85491fff4b0c1867d713492c5188842d130507f8
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-21 20:36:28 -0300

    Dynamic title per page

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head';
+
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
     <h1 className={styles.title}>
-      Hello World
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
     </h1>
   )
 }
