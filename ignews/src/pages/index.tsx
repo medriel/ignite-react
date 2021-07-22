142d773155aafb90742dd41d1c2622fbc4a00d0f
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-22 19:37:02 -0300

    Global App Styles

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss'
-
 export default function Home() {
   return (
-    <h1 className={styles.title}>
+    <>
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-    </h1>
+      <h1>
+        Hello world!
+      </h1>
+    </>
   )
 }
