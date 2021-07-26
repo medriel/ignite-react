8b809d6c5c98fbdb8442bdae366d6a55fd63c408
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-26 20:50:09 -0300

    Parametrização nas rotas

---
+++ ignews/src/pages/api/users/[id].tsx
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+/* Formas de autenticação */
+// JWT (Storage)
+// Next Auth (Social)
+// Cognito, Auth0
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+
+  console.log(request.query)
+
+  const users = [
+    { id: 1, name: 'Adriel' },
+    { id: 2, name: 'Marcela' },
+    { id: 3, name: 'Yasmin' },
+    { id: 4, name: 'Izaulina' },
+    { id: 5, name: 'André' },
+  ]
+}
