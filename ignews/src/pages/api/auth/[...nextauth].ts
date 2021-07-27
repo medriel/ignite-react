d5318aaeb78670fd5a8b5c5ac5722565aa793c74
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-27 20:39:24 -0300

    removendo chave privada JWT

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -13,9 +13,6 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
-  jwt: {
-    signingKey: process.env.SIGNING_KEY,
-  },
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user
