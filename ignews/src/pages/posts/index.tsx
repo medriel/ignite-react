250798c2f04df56b2289a465e10412e9bce103d2
Adriel Medeiros <adriel.souza397@gmail.com>
2021-08-01 09:41:49 -0300

    Listando posts em tela

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -1,10 +1,22 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,26 +25,13 @@ export default function Posts() {
 
       <main className={styles.container} >
         <div className={styles.posts} >
-          <a href="#">
-            <time>31 de julho de 2021</time>
-            <strong>Breve descrição</strong>
-            <p>Conteudo que vai ter no posto, vem todos aqui formando um paragrafo, aaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-          </a>
-          <a href="#">
-            <time>31 de julho de 2021</time>
-            <strong>Breve descrição</strong>
-            <p>Conteudo que vai ter no posto, vem todos aqui formando um paragrafo, aaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-          </a>
-          <a href="#">
-            <time>31 de julho de 2021</time>
-            <strong>Breve descrição</strong>
-            <p>Conteudo que vai ter no posto, vem todos aqui formando um paragrafo, aaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-          </a>
-          <a href="#">
-            <time>31 de julho de 2021</time>
-            <strong>Breve descrição</strong>
-            <p>Conteudo que vai ter no posto, vem todos aqui formando um paragrafo, aaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -50,9 +49,22 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   )
 
-  console.log(JSON.stringify(response, null, 2))
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  });
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
