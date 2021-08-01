1a6b39b0d4bf4e52b0a553133fbcb3884c3a6dea
Adriel Medeiros <adriel.souza397@gmail.com>
2021-08-01 16:14:33 -0300

    Validando assinatura ativa

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn } from 'next-auth/client';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
@@ -9,12 +10,19 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter();
 
   async function handleSubscribe() {
     if (!session) {
       signIn('github')
       return;
     }
+
+    if (session.activeSubscription) {
+      router.push('/posts');
+      return;
+    }
+
     //criação da checkout session
     try {
       const response = await api.post('subscribe')
--- ignews/src/pages/posts/[slug].tsx
+++ ignews/src/pages/posts/[slug].tsx
@@ -36,11 +36,17 @@ export default function Post({ post }) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  const session = await getSession({ req })
+  const session = await getSession({ req });
   const { slug } = params;
 
-  // if(!session){
-  // }
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req)
 
