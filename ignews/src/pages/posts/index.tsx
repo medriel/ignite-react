a7e00e720468596da066c8f035932e387b041e5c
Adriel Medeiros <adriel.souza397@gmail.com>
2021-08-01 15:12:56 -0300

    Página: Post

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -4,6 +4,7 @@ import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss'
+import Link from 'next/link';
 
 type Post = {
   slug: string;
@@ -26,11 +27,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container} >
         <div className={styles.posts} >
           {posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`} >
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
