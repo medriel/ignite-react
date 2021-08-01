8d1b67de1d353183c3dd12dc7e356df51e151543
Adriel Medeiros <adriel.souza397@gmail.com>
2021-08-01 09:58:59 -0300

    Navegação no menu

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
+import Link from 'next/link'
 
 export function Header() {
   return (
@@ -7,8 +8,12 @@ export function Header() {
       <div className={styles.headerContent}>
         {/* <img src="/images/logo.svg" alt="ig.news" ></img> */}
         <nav>
-          <a className={styles.active} >Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active} >Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
+            <a>Posts</a>
+          </Link>
         </nav>
         <SignInButton />
       </div>
