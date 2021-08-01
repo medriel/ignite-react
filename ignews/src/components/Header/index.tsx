52ddb02b76c10fb242a27a242d0505d43178b94f
Adriel Medeiros <adriel.souza397@gmail.com>
2021-08-01 10:49:52 -0300

    Componente: ActiveLink

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,19 +1,20 @@
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
-import Link from 'next/link'
+import { ActiveLink } from '../ActiveLink'
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        {/* <img src="/images/logo.svg" alt="ig.news" ></img> */}
+        <img src="/images/logo.svg" alt="ig.news" ></img>
         <nav>
-          <Link href="/">
-            <a className={styles.active} >Home</a>
-          </Link>
-          <Link href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
