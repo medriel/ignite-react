09472ec2fff3ec21be94e33b29fbf63ec043fcba
Adriel Medeiros <adriel.souza397@gmail.com>
2021-08-03 08:56:30 -0300

    Gerando previews estáticos

--- ignews/src/pages/posts/preview/[slug].tsx
+++ ignews/src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { getSession, useSession } from "next-auth/client";
 import Head from "next/head";
 import Link from "next/link";
@@ -55,10 +55,10 @@ export default function PostPreview({ post }: PostPreviewProps) {
   );
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = () => {
   return {
     paths: [],
-    fallback: 'blocking'
+    fallback: 'blocking' //true, false, blocking
   }
 }
 
@@ -82,6 +82,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       post,
-    }
+    },
+    redirect: 60 * 30, //30 minutes
   }
 }
