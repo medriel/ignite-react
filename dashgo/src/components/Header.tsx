bfc74eea1eb3fea393911a2c0d45637c1ce1c870
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-08-22 11:05:30 -0300

    Finalizando Header

--- dashgo/src/components/Header.tsx
+++ dashgo/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -46,6 +46,36 @@ export function Header() {
         />
         <Icon as={RiSearchLine} fontSize="20" />
       </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Adriel Medeiros</Text>
+            <Text color="gray.300" fontSize="small">
+              adriel@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="Adriel Medeiros" src="" />
+        </Flex>
+      </Flex>
+
     </Flex>
   );
 }
