93744883c1491715329a84f194426705f0513904
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-09-04 08:54:11 -0300

    Sidebar responsiva

--- dashgo/src/components/Header/index.tsx
+++ dashgo/src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, useBreakpointValue, IconButton, Icon } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header() {
+
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +26,19 @@ export function Header() {
       px="6"
       align="center"
     >
+
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        >
+
+        </IconButton>
+      )}
 
       <Logo />
 
