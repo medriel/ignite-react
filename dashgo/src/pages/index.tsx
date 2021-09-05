93520a4e2cba7dd72c6d58fe72f8dc37b45b8d91
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-09-05 08:56:39 -0300

    Form de autenticação

--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,7 +1,24 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Input } from '../components/Form/Input'
 
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
+
+  const { register, handleSubmit, formState } = useForm()
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000)) //utilizado para definir o tempo que o botao de `entrar` fica carregando
+
+    console.log(values)
+
+    //formState. // -> da para saber algumas informações interessantes do formulario
+  }
+
   return (
     <Flex
       w="100vw"
@@ -16,12 +33,21 @@ export default function SignIn() {
         p="8" //32 px / 2rem
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="Email" />
-          <Input name="password" type="password" label="Senha" />
+          <Input name="email" type="email" label="Email" {...register('email')} />
+          <Input name="password" type="password" label="Senha" {...register('password')} />
         </Stack>
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting} // possibilita colocar o sinal de carregamento no botao
+        >
+          Entrar
+        </Button>
       </Flex>
     </Flex>
   )
