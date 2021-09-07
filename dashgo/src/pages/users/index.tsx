7c5e82e36e6f84c0d3825f8219ca1d75211fedfa
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-09-07 17:20:49 -0300

    Factories e seeds

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { Flex, Box, Heading, Button, Icon, Thead, Tr, Th, Checkbox, Tbody, Td, Text, Table, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Header } from "../../components/Header";
@@ -11,6 +12,12 @@ export default function UserList() {
     base: false,
     lg: true,
   })
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
 
   return (
     <Box>
