5d3e7bf5bfb9ac49a3e51b51ef94954ad248d9ca
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-09-13 20:53:03 -0300

    Configurando axios

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -6,12 +6,12 @@ import { Sidebar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
 import { useQuery } from 'react-query'
 import Link from 'next/link';
+import { api } from '../../services/api';
 
 export default function UserList() {
 
   const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+    const { data } = await api.get('users')
 
     const users = data.users.map(user => {
       return {
