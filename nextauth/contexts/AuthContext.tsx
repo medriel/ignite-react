23ed1c39a95a13ddde23da8aaf5bc4f5befdd088
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-09-30 08:09:33 -0300

    Salvando dados do usuário

--- nextauth/contexts/AuthContext.tsx
+++ nextauth/contexts/AuthContext.tsx
@@ -1,13 +1,21 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
+import Router from "next/router"
+
+type User = {
+  email: string
+  permissions: string[]
+  roles: string[]
+}
 
 type SignInCredentials = {
-  email: String;
-  password: String;
+  email: string;
+  password: string;
 }
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>
+  user: User
   isAuthenticated: boolean
 }
 
@@ -18,7 +26,8 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData)
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>()
+  const isAuthenticated = !!user;
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -26,13 +35,24 @@ export function AuthProvider({ children }: AuthProviderProps) {
         email,
         password,
       })
+
+      const { token, refreshToken, permissions, roles } = response.data
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      })
+
+      Router.push('./dashboard')
+
     } catch (err) {
       console.log(err)
     }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   )
