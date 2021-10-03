1c3df180ab70d6289d230d6137e941e9612e23cb
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-10-03 09:02:07 -0300

    Realizando refresh do token

--- nextauth/services/api.ts
+++ nextauth/services/api.ts
@@ -1,11 +1,45 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies()
+let cookies = parseCookies()
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
     Authorization: `Bearer ${cookies['nextauth.token']}`
+  }
+})
+
+api.interceptors.response.use(response => {
+  return response
+}, (error: AxiosError) => {
+  if (error.response.status === 401) {
+    if (error.response.data?.code === 'token.expired') {
+      //renovar o token
+      cookies = parseCookies();
+
+      const { 'nextauth.refreshToken': refreshToken } = cookies
+
+      api.post('/refresh', {
+        refreshToken,
+      }).then(response => {
+        const { token } = response.data
+
+        //salvando informações atualizadas nos cookies
+        setCookie(undefined, 'nextauth.token', token, {
+          maxAge: 60 * 60 * 24 * 30, //30 dias
+          path: '/'
+        })
+        setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+          maxAge: 60 * 60 * 24 * 30, //30 dias
+          path: '/'
+        })
+
+        api.defaults.headers['Authorization'] = `Bearer ${token}`
+      })
+    } else {
+      //deslogar o usuario
+
+    }
   }
 })
