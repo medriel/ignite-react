546f1d28544da21efcab377b37f85d94f8c17cbd
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-10-03 09:31:39 -0300

    Fila de requisições no Axios

--- nextauth/services/api.ts
+++ nextauth/services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from 'axios'
 import { parseCookies, setCookie } from 'nookies'
 
 let cookies = parseCookies()
+let isRefreshing = false
+let failedRequestQueue = []
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -19,23 +21,49 @@ api.interceptors.response.use(response => {
       cookies = parseCookies();
 
       const { 'nextauth.refreshToken': refreshToken } = cookies
+      const originalConfig = error.config
 
-      api.post('/refresh', {
-        refreshToken,
-      }).then(response => {
-        const { token } = response.data
+      if (!isRefreshing) {
+        isRefreshing = true
 
-        //salvando informações atualizadas nos cookies
-        setCookie(undefined, 'nextauth.token', token, {
-          maxAge: 60 * 60 * 24 * 30, //30 dias
-          path: '/'
+        api.post('/refresh', {
+          refreshToken,
+        }).then(response => {
+          const { token } = response.data
+
+          //salvando informações atualizadas nos cookies
+          setCookie(undefined, 'nextauth.token', token, {
+            maxAge: 60 * 60 * 24 * 30, //30 dias
+            path: '/'
+          })
+          setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+            maxAge: 60 * 60 * 24 * 30, //30 dias
+            path: '/'
+          })
+
+          api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+          failedRequestQueue.forEach(request => request.resolve(token))
+          failedRequestQueue = []
+        }).catch(err => {
+          failedRequestQueue.forEach(request => request.reject(err))
+          failedRequestQueue = []
+        }).finally(() => {
+          isRefreshing = false
         })
-        setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
-          maxAge: 60 * 60 * 24 * 30, //30 dias
-          path: '/'
-        })
+      }
 
-        api.defaults.headers['Authorization'] = `Bearer ${token}`
+      return new Promise((resolve, reject) => {
+        failedRequestQueue.push({
+          resolve: (token: string) => {
+            originalConfig.headers['Authorization'] = `Bearer ${token}`
+
+            resolve(api(originalConfig))
+          },
+          reject: (err: AxiosError) => {
+            reject(err)
+          }
+        })
       })
     } else {
       //deslogar o usuario
