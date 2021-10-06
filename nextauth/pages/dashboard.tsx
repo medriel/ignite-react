bdcc3569269adcae48ba9b7bb782a5a8d808bc7a
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-10-05 22:00:31 -0300

    Broadcast de logout

--- nextauth/pages/dashboard.tsx
+++ nextauth/pages/dashboard.tsx
@@ -8,7 +8,7 @@ import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
 
-  const { user, isAuthenticated } = useContext(AuthContext)
+  const { user, signOut, isAuthenticated } = useContext(AuthContext)
 
   useEffect(() => {
     api.get('/me')
@@ -19,6 +19,9 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign out</button>
+
       <Can permissions={['metrics.list']}>
         <div>Metricas</div>
       </Can>
