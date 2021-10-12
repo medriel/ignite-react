6ddb8a720cb908598772cc077c9d4661011d21c7
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-10-12 09:17:56 -0300

    Virtualização

--- perfreact/components/SearchResults.tsx
+++ perfreact/components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { List, ListRowRenderer } from 'react-virtualized'
 import { ProductItem } from "./ProductItem"
 
 interface SearchResultsProps {
@@ -13,11 +13,31 @@ interface SearchResultsProps {
 }
 
 export function SearchResult({ totalPrice, results, onAddToWishlist }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishlist={onAddToWishlist}
+        />
+      </div>
+    )
+  }
 
   return (
     <div>
       <h3>{totalPrice}</h3>
-      {results.map(product => {
+
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+
+      {/* {results.map(product => {
         return (
           <ProductItem
             key={product.id}
@@ -25,7 +45,7 @@ export function SearchResult({ totalPrice, results, onAddToWishlist }: SearchRes
             onAddToWishlist={onAddToWishlist}
           />
         )
-      })}
+      })} */}
     </div>
   )
 }
