143e9dadb0fcf10ebba26aeda81785906753e218
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-10-12 21:12:02 -0300

    Configurando Testing Library

--- ignews/src/pages/_app.tsx
+++ ignews/src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from 'next/app'
-import { Header } from '../components/Header'
+import { Header } from '../components/Header';
 import { Provider as NextAuthProvider } from 'next-auth/client'
 
-import '../styles/global.scss'
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
