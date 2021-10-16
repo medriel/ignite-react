70e433d238490272e5955a8b4c94ab1553f2fa3c
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-10-16 10:56:47 -0300

    Ajustando teste da página do post

--- ignews/src/tests/pages/Post.spec.tsx
+++ ignews/src/tests/pages/Post.spec.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react'
-import Post, { getServerSideProps } from '../../pages/posts/[slug]'
-import { mocked } from 'ts-jest/utils'
 import { getPrismicClient } from '../../services/prismic'
+import { mocked } from 'ts-jest/utils'
+import Post, { getServerSideProps } from '../../pages/posts/[slug]'
 import { getSession } from 'next-auth/client'
 
 const post = {
   slug: 'my-new-post',
-  title: 'My new post',
+  title: 'my-new-post',
   content: '<p>Post excerpt</p>',
   updatedAt: '16 de Outubro'
 }
@@ -14,45 +14,76 @@ const post = {
 jest.mock('next-auth/client')
 jest.mock('../../services/prismic')
 
-describe('Post page', () => {
-  it('renders correctly', () => {
-    render(<Post post={post} />)
+const renderPost = () => {
+  render(<Post post={post} />)
+}
 
-    expect(screen.getByText("My new post")).toBeInTheDocument()
-    expect(screen.getByText("Post excerpt")).toBeInTheDocument()
+describe('Post page', () => {
+  it('should render correctly', () => {
+    renderPost()
+    expect(screen.getByText('my-new-post')).toBeInTheDocument()
+    expect(screen.getByText('Post excerpt')).toBeInTheDocument()
   })
 
   it('redirects user if no subscription is found', async () => {
     const getSessionMocked = mocked(getSession)
-    const getPrismicClientMocked = mocked(getPrismicClient)
-
-    getSessionMocked.mockResolvedValueOnce({
-      activeSubscription: null
-    } as any)
-    getPrismicClientMocked.mockReturnValueOnce({
-      getByUID: jest.fn().mockResolvedValueOnce({})
-    } as any)
+    getSessionMocked.mockResolvedValueOnce(null)
 
     const response = await getServerSideProps({
       params: {
         slug: 'my-new-post'
-      },
+      }
     } as any)
 
     expect(response).toEqual(
       expect.objectContaining({
         redirect: expect.objectContaining({
-          destination: '/',
+          destination: '/'
         })
       })
     )
   })
 
   it('loads initial data', async () => {
+    const getPrismicClientMocked = mocked(getPrismicClient)
     const getSessionMocked = mocked(getSession)
 
-    getSessionMocked.mockResolvedValueOnce({
-      activeSubscription: 'fake-active-subscription'
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [
+            { type: 'heading', text: 'My new post' }
+          ],
+          content: [
+            { type: 'paragraph', text: 'Post excerpt' }
+          ]
+        },
+        last_publication_date: '04-01-2021'
+      })
     } as any)
+
+    getSessionMocked.mockResolvedValueOnce({
+      activeSubscription: 'fake_subscription'
+    } as any)
+
+    const response = await getServerSideProps({
+      params: {
+        slug: 'my-new-post'
+      }
+    } as any)
+
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: 'my-new-post',
+            title: 'My new post',
+            content: '<p>Post excerpt</p>',
+            updatedAt: '01 de abril de 2021'
+          }
+        }
+      })
+    )
   })
 })
