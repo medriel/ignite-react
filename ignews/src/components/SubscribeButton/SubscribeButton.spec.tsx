1b86a82b0e0055be365f552ad5c175f8b2032dc5
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-10-15 21:04:26 -0300

    ajustando teste do componente subscribeButton

--- ignews/src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ ignews/src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -62,6 +62,6 @@ describe('SubscribeButton component', () => {
 
     fireEvent.click(subscribeButton)
 
-    expect(pushMock).toHaveBeenCalled()
+    expect(pushMock).toHaveBeenCalledWith('/posts')
   })
 })
